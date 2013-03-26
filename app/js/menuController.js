82cc13dd3c13a391c6009fd99d40779bc862e5d5
swanmo <swanmo@gmail.com>
2013-03-26 10:23:09 +0100

    Minor celanup

--- app/js/menuController.js
+++ app/js/menuController.js
@@ -5,8 +5,8 @@
 function MenuCtrl($scope, Recipes, MultiTagUtil) {
   	$scope.recipes = Recipes.query();
   	$scope.menus = Recipes.query();
-  	$scope.noOfCourses = 1;
-  	$scope.foo = {name: "Umur"};
+  	$scope.noOfCourses = 4;
+  	$scope.rating = 0;
 	// $scope.allTags = {tags: Recipes.getAllTagsInUse(), tags2: Recipes.getAllTagsInUse(), selected:[], selected2:[]};
 	$scope.selectableTags = Recipes.getAllTagsInUse();
 	$scope.includingSelections = [];
@@ -14,15 +14,15 @@ function MenuCtrl($scope, Recipes, MultiTagUtil) {
 	var descriptionTemplate = 'partials/template-menu-intro.htm';
 
 	$scope.setupSelections = function() {
-		$scope.newSelectionInclude();
+		$scope.newSelectionInclude(["vegetariskt"]);
 		$scope.newSelectionExclude();
 	}
 
 	/* <tagmanager tags="selection.tags" selectable="selection.selectable" inputid="kalle01"></tagmanager> */
-	$scope.newSelectionInclude = function() {
+	$scope.newSelectionInclude = function(arrSuggestions) {
 		$scope.includingSelections.push(
 			{numberOfRecipes:"minst 1",
-			tags:{selected:["varan"]},
+			tags:{selected:arrSuggestions},
 			id:'sel1_' + ($scope.includingSelections.length + 1),
 			selectable: $scope.selectableTags});
 	}
@@ -30,7 +30,7 @@ function MenuCtrl($scope, Recipes, MultiTagUtil) {
 	$scope.newSelectionExclude = function() {
 		$scope.excludingSelections.push({
 			numberOfRecipes:"något",
-			tags:{selected:["fett", "kalle"]},
+			tags:{selected:[]},
 			id:'sel2_'+ ($scope.excludingSelections.length + 1),
 			selectable: $scope.selectableTags});
 	}
@@ -53,13 +53,15 @@ function MenuCtrl($scope, Recipes, MultiTagUtil) {
   	}
 
   	$scope.createMenu = function() {
+  		// $scope.noOfCourses
+  		alert($scope.rating);
   		var str = "Including:\n";
   		for (var i = $scope.includingSelections.length - 1; i >= 0; i--) {
-  			str += " including id: " + $scope.includingSelections[i].id + ",\n tags: " + $scope.includingSelections[i].tags.selected + "(" +$scope.includingSelections[i].tags.selected.selected+ ")";
+  			str += " \nincluding id: " + $scope.includingSelections[i].id + ", number:" + $scope.includingSelections[i].numberOfRecipes + ", tags: " + $scope.includingSelections[i].tags.selected + "(" +$scope.includingSelections[i].tags.selected.selected+ ")";
   		};
 		str += "\nExcluding:\n";
   		for (var i = $scope.excludingSelections.length - 1; i >= 0; i--) {
-  			str += " excluding id: " + $scope.excludingSelections[i].id + ",\n tags: " + $scope.excludingSelections[i].tags.selected;
+  			str += " \nexcluding id: " + $scope.excludingSelections[i].id + ", number:" + $scope.excludingSelections[i].numberOfRecipes +", tags: " + $scope.excludingSelections[i].tags.selected;
   		};
   		alert(str);	
   	}
